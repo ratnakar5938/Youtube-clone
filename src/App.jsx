351429e7bc6fb4f502186c8dbd6f75b0d910ca9e
Ratnakar <ratnakar3895@yahoo.com>
2021-10-20 11:17:55 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        // BEM class naming convention
+        <div className="app">
+            <h1>Hello everyone! Lets build youtube clone🚀</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyDOlqJxSOUdHUiU5mxspZ7dp_apE59FSMw",
+    authDomain: "clone-9d2e6.firebaseapp.com",
+    projectId: "clone-9d2e6",
+    storageBucket: "clone-9d2e6.appspot.com",
+    messagingSenderId: "103074615888",
+    appId: "1:103074615888:web:e4f9e597b89892a4aeb755",
+    measurementId: "G-RBGX5NMFYJ",
+};
